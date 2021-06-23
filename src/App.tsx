37a31ffdb2deae6920f845dc4e724ddb5610f69a
José Carlos Martins <jcmartins81@outlook.com>
2021-06-22 22:35:33 -0300

    configurnado regras no firebase e ajustando elementos da room

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
 import {Home} from './pages/Home'
 import {NewRoom} from './pages/NewRoom'
 import { AuthContextProvider } from './contexts/AuthContext'
+import {Room} from "./pages/Room";
 
 function App() {
   return (
     <BrowserRouter>
         <AuthContextProvider>
+            <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/new" exact component={NewRoom} />
+          <Route path="/rooms/:id" component={Room}/>
+            </Switch>
         </AuthContextProvider>
     </BrowserRouter>
   );
---
+++ src/pages/Room.tsx
@@ -0,0 +1,33 @@
+import logoImg from '../assets/images/logo.svg'
+import {Button} from "../components/Button";
+
+export function Room() {
+    return (
+        <div id='page-room'>
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="letmeask"/>
+                    <div>código</div>
+                </div>
+            </header>
+
+            <main className="content">
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form>
+                    <textarea
+                        placeholder="O que você quer saber?"
+                    />
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta, <button>Faça seu Login</button>.</span>
+                        <Button type="submit">Enviar Pergunta</Button>
+                    </div>
+                </form>
+
+            </main>
+        </div>
+    )
+}
